4385dd125c4f128cd378e1bc6813b6f7de30c842
a23gabst <a23gabst@student.his.se>
2024-03-28 12:05:37 +0100

    Linearlayout imageview edittext button arbete är på gång

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.widgets;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.mybtn);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","We clicked on Tomten!");
+            }
+        });
     }
+
+
 }
